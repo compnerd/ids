2ebbb862288fa10aeba7eb70a8b5f0b9302d7e20
Saleem Abdulrasool <compnerd@compnerd.org>
2024-01-21 17:19:27 -0800

    Update IgnoredFunctions.hh
    Apply style suggestions.

--- Tests/IgnoredFunctions.hh
+++ Tests/IgnoredFunctions.hh
@@ -1,11 +1,11 @@
-// RUN: %idt -export-macro IDT_TEST_ABI -ignore foo,bar %s 2>&1 | %FileCheck %s
-// RUN: %idt -export-macro IDT_TEST_ABI -ignore foo -ignore bar %s 2>&1 | %FileCheck %s
+// RUN: %idt -ignore foo,bar %s 2>&1 | %FileCheck %s
+// RUN: %idt -ignore foo -ignore bar %s 2>&1 | %FileCheck %s
 
-void foo() noexcept;
-// CHECK-NOT: IgnoredFunctions.hh:[[@LINE-1]]:1: remark: unexported public interface 'foo'
+void f() noexcept;
+// CHECK-NOT: IgnoredFunctions.hh:[[@LINE-1]]:1: remark: unexported public interface 'f'
 
-int bar(int x);
-// CHECK-NOT: IgnoredFunctions.hh:[[@LINE-1]]:1: remark: unexported public interface 'bar'
+int g(int x);
+// CHECK-NOT: IgnoredFunctions.hh:[[@LINE-1]]:1: remark: unexported public interface 'g'
 
-const char* snaf(int count);
-// CHECK: IgnoredFunctions.hh:[[@LINE-1]]:1: remark: unexported public interface 'snaf'
+const char* h(int count);
+// CHECK: IgnoredFunctions.hh:[[@LINE-1]]:1: remark: unexported public interface 'h'
