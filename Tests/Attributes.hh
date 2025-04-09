37ed155c3334488f47ac260f5c0cc79d7a2cb8ce
Andrew Rogers <andrurogerz@gmail.com>
2025-04-09 10:14:26 -0700

    additional test cases with different attributes and attribute positions

--- Tests/Attributes.hh
+++ Tests/Attributes.hh
@@ -8,26 +8,47 @@
 #define IMPORT_ABI __attribute__((visibility("default")))
 #endif
 
-EXPORT_ABI void functionDLLExport();
+EXPORT_ABI void functionDLLExport1();
 // CHECK-NOT: Attributes.hh:[[@LINE-1]]:{{.*}}
 
-EXPORT_ABI extern unsigned variableDLLExport;
+void EXPORT_ABI functionDLLExport2();
 // CHECK-NOT: Attributes.hh:[[@LINE-1]]:{{.*}}
 
-IMPORT_ABI void functionDLLImport();
+EXPORT_ABI extern unsigned variableDLLExport1;
 // CHECK-NOT: Attributes.hh:[[@LINE-1]]:{{.*}}
 
-IMPORT_ABI extern unsigned variableDLLImport;
+extern EXPORT_ABI unsigned variableDLLExport2;
 // CHECK-NOT: Attributes.hh:[[@LINE-1]]:{{.*}}
 
-__attribute__((visibility("default"))) void functionAttributeVisibilityDefault();
+IMPORT_ABI void functionDLLImport1();
 // CHECK-NOT: Attributes.hh:[[@LINE-1]]:{{.*}}
 
-__attribute__((visibility("default"))) extern unsigned variableAttributeVisibilityDefault;
+void IMPORT_ABI functionDLLImport2();
 // CHECK-NOT: Attributes.hh:[[@LINE-1]]:{{.*}}
 
-__attribute__((visibility("hidden"))) void functionAttributeVisibilityHidden();
-// CHECK: Attributes.hh:[[@LINE-1]]:{{.*}}: remark: unexported public interface 'functionAttributeVisibilityHidden'
+IMPORT_ABI extern unsigned variableDLLImport1;
+// CHECK-NOT: Attributes.hh:[[@LINE-1]]:{{.*}}
+
+extern IMPORT_ABI unsigned variableDLLImport2;
+// CHECK-NOT: Attributes.hh:[[@LINE-1]]:{{.*}}
+
+__attribute__((visibility("default"))) void functionAttributeVisibilityDefault1();
+// CHECK-NOT: Attributes.hh:[[@LINE-1]]:{{.*}}
+
+void __attribute__((visibility("default"))) functionAttributeVisibilityDefault2();
+// CHECK-NOT: Attributes.hh:[[@LINE-1]]:{{.*}}
+
+__attribute__((visibility("default"))) extern unsigned variableAttributeVisibilityDefault1;
+// CHECK-NOT: Attributes.hh:[[@LINE-1]]:{{.*}}
+
+extern __attribute__((visibility("default"))) unsigned variableAttributeVisibilityDefault2;
+// CHECK-NOT: Attributes.hh:[[@LINE-1]]:{{.*}}
+
+__attribute__((visibility("hidden"))) void functionAttributeVisibilityHidden1();
+// CHECK: Attributes.hh:[[@LINE-1]]:{{.*}}: remark: unexported public interface 'functionAttributeVisibilityHidden1'
+
+void __attribute__((visibility("hidden"))) functionAttributeVisibilityHidden2();
+// CHECK: Attributes.hh:[[@LINE-1]]:{{.*}}: remark: unexported public interface 'functionAttributeVisibilityHidden2'
 
 [[gnu::visibility("default")]] void functionGnuVisibilityDefault();
 // CHECK-NOT: Attributes.hh:[[@LINE-1]]:{{.*}}
@@ -37,3 +58,21 @@ __attribute__((visibility("hidden"))) void functionAttributeVisibilityHidden();
 
 [[gnu::visibility("hidden")]] extern unsigned variableGnuVisibilityHidden;
 // CHECK: Attributes.hh:[[@LINE-1]]:{{.*}}: remark: unexported public interface 'variableGnuVisibilityHidden'
+
+[[deprecated]] extern unsigned variableDeprecated;
+// CHECK: Attributes.hh:[[@LINE-1]]:{{.*}}: remark: unexported public interface 'variableDeprecated'
+
+[[deprecated]] void functionDeprecated();
+// CHECK: Attributes.hh:[[@LINE-1]]:{{.*}}: remark: unexported public interface 'functionDeprecated'
+
+__attribute__((unused)) extern unsigned variableUnused1;
+// CHECK: Attributes.hh:[[@LINE-1]]:{{.*}}: remark: unexported public interface 'variableUnused1'
+
+extern __attribute__((unused)) unsigned variableUnused2;
+// CHECK: Attributes.hh:[[@LINE-1]]:{{.*}}: remark: unexported public interface 'variableUnused2'
+
+__attribute__((unused)) void functionUnused1();
+// CHECK: Attributes.hh:[[@LINE-1]]:{{.*}}: remark: unexported public interface 'functionUnused1'
+
+void __attribute__((unused)) functionUnused2();
+// CHECK: Attributes.hh:[[@LINE-1]]:{{.*}}: remark: unexported public interface 'functionUnused2'
