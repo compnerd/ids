b9ad39408eb721836449a58a3553daa2a4d9724d
Andrew Rogers <andrurogerz@gmail.com>
2025-04-28 14:11:18 -0700

    additional template class tests cases w/out virtual methods

--- Tests/TemplateClasses.hh
+++ Tests/TemplateClasses.hh
@@ -1,25 +1,43 @@
 // RUN: %idt --export-macro=IDT_TEST_ABI %s 2>&1 | %FileCheck %s
 
 // CHECK-NOT: TemplateClasses.hh:[[@LINE+1]]:{{.*}}
-template <typename T, typename U> struct TemplateClass {
+template <typename T, typename U> struct TemplateClassVirtual {
   // CHECK-NOT: TemplateClasses.hh:[[@LINE+1]]:{{.*}}
   virtual void virtualMethod();
+};
+
+// CHECK-NOT: TemplateClasses.hh:[[@LINE+1]]:{{.*}}
+template <typename U> struct TemplateClassVirtual<int, U> {
+  // CHECK-NOT: TemplateClasses.hh:[[@LINE+1]]:{{.*}}
+  virtual void virtualMethod();
+};
+
+// CHECK-NOT: TemplateClasses.hh:[[@LINE+1]]:{{.*}}
+extern template class TemplateClassVirtual<long, void*>;
+
+// CHECK-NOT: TemplateClasses.hh:[[@LINE+1]]:{{.*}}
+template class TemplateClassVirtual<unsigned, long>;
+
+// CHECK: TemplateClasses.hh:[[@LINE+1]]:20: remark: unexported public interface 'TemplateClassVirtual<int, long>'
+template <> struct TemplateClassVirtual<int, long> {
+  // CHECK-NOT: TemplateClasses.hh:[[@LINE+1]]:{{.*}}
+  virtual void virtualMethod();
+};
+
+// CHECK-NOT: TemplateClasses.hh:[[@LINE+1]]:{{.*}}
+template <typename T, typename U> struct TemplateClass {
+  // CHECK-NOT: TemplateClasses.hh:[[@LINE+1]]:{{.*}}
+  void method();
 };
 
 // CHECK-NOT: TemplateClasses.hh:[[@LINE+1]]:{{.*}}
 template <typename U> struct TemplateClass<int, U> {
   // CHECK-NOT: TemplateClasses.hh:[[@LINE+1]]:{{.*}}
-  virtual void virtualMethod();
+  void method();
 };
 
 // CHECK-NOT: TemplateClasses.hh:[[@LINE+1]]:{{.*}}
-extern template class TemplateClass<long, void*>;
-
-// CHECK-NOT: TemplateClasses.hh:[[@LINE+1]]:{{.*}}
-template class TemplateClass<unsigned, long>;
-
-// CHECK: TemplateClasses.hh:[[@LINE+1]]:20: remark: unexported public interface 'TemplateClass<int, long>'
 template <> struct TemplateClass<int, long> {
-  // CHECK-NOT: TemplateClasses.hh:[[@LINE+1]]:{{.*}}
-  virtual void virtualMethod();
+  // CHECK: TemplateClasses.hh:[[@LINE+1]]:3: remark: unexported public interface 'method'
+  void method();
 };
