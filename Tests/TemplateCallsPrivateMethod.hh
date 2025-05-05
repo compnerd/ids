51a8f0673123874777ed98cd836f0239ba95a250
Andrew Rogers <andrurogerz@gmail.com>
2025-05-05 09:32:43 -0700

    handle calls to unresolved member methods

---
+++ Tests/TemplateCallsPrivateMethod.hh
@@ -0,0 +1,20 @@
+// RUN: %idt --export-macro IDT_TEST_ABI %s 2>&1 | %FileCheck %s
+
+class TemplateCallsPrivateMethod {
+public:
+  // CHECK-NOT: TemplateCallsPrvateMethod.hh:[[@LINE+1]]:{{.*}}
+  template <typename T> void publicTemplateMethod(T x) {
+    privateMethodForTemplate(x);
+  }
+
+private:
+  // NOTE: we use CHECK-DAG here because these remarks may come out of order and
+  // we cannot control the order by rearranging members.
+
+  // CHECK-DAG: TemplateCallsPrivateMethod.hh:[[@LINE+1]]:3: remark: unexported public interface 'privateMethodForTemplate'
+  void privateMethodForTemplate(long x) const;
+
+  // CHECK-DAG: TemplateCallsPrivateMethod.hh:[[@LINE+1]]:3: remark: unexported public interface 'privateMethodForTemplate'
+  void privateMethodForTemplate(int x) const;
+};
+
